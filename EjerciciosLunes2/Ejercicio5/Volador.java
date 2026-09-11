e8be42c28bd0a341e3066da2221b9732c51ecf99
TheNinja1542 <estefanyvergara4@gmail.com>
2026-09-10 21:07:58 -0500

    cambio volador

--- EjerciciosLunes2/Ejercicio5/Volador.java
+++ EjerciciosLunes2/Ejercicio5/Volador.java
@@ -3,6 +3,6 @@ package EjerciciosLunes2.Ejercicio5;
 public interface Volador {
 
     default String volar() {
-        return "El animal está volando.";
+        return "El animal esta volando.";
     }
 }
